8f3e4e801ae44c7db127883025ddd15e87b6c165
Robinson Regalado <rregalado17@gmail.com>
2019-09-03 04:19:31 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,9 +1,90 @@
 require_relative "../config/environment.rb"
 
 class Student
-
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
 
+  attr_accessor :name, :grade, :id 
+  
+  def initialize(name, grade, id=nil)
+    @name = name
+    @grade = grade 
+    @id = id
+  end
+  
+  def self.create_table 
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS students (
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      grade INTEGER
+    )
+    SQL
 
+    DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+        sql = <<-SQL
+      DROP TABLE students
+    SQL
+
+    DB[:conn].execute(sql)
+  end
+
+
+  def save 
+    if self.id
+      sql = <<-SQL
+        UPDATE students
+        SET name = ?,
+        grade = ?
+        WHERE id = ?
+      SQL
+      
+    DB[:conn].execute(sql, self.name, self.grade, self.id )
+    
+    else 
+      sql = <<-SQL 
+      INSERT INTO students (name, grade)
+      VALUES (?, ?);
+      SQL
+    
+      DB[:conn].execute(sql, self.name, self.grade)
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+    end 
+  end
+  
+  def self.create(name, grade)
+    student = Student.new(name, grade)
+    student.save
+    student
+  end
+  
+  def self.new_from_db(row)
+    id = row[0]
+    name = row[1]
+    grade = row[2]
+    new_student = self.new(name, grade, id) #define the row properties first and then create the instance
+  end
+  
+ def self.find_by_name(name)
+   sql = <<-SQL 
+   SELECT *
+   FROM students
+   WHERE name = ?
+   LIMIT 1
+   SQL
+   
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)  #map the name in the row to create a new instance from the DB and return the first result
+    end.first
+ end
+  
+  def update 
+    sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.grade, self.id)
+  end
+  
+  
 end
